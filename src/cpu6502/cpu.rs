344dcbfff906e60eac9d2543569269c589e7a9c6
Dogue <dogue@oddhelix.com>
2023-06-04 18:27:11 -0400

    fix: indexing bug in load_program causing panic

--- src/cpu6502/cpu.rs
+++ src/cpu6502/cpu.rs
@@ -1,6 +1,6 @@
 use rustbucket::Register;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Cpu6502 {
     pub a: Register<u8>,
     pub x: Register<u8>,
@@ -18,22 +18,64 @@ impl Cpu6502 {
 
     pub fn with_program(program: Vec<u8>) -> Self {
         let mut cpu = Self::default();
-        cpu.memory = vec![0; 0xFFFF];
         cpu.load_program(program);
         cpu
     }
 
-    fn load_program(&mut self, program: Vec<u8>) {
-        // Program length can vary, so we can't just
-        // .splice() as that shrinks the memory Vec
-        let mut i: usize = 0x8000;
-        for v in program {
-            self.memory[i] = v;
-            i += 1;
-        }
+    pub fn load_program(&mut self, program: Vec<u8>) {
+        // splice() will remove n elements, even if n is larger than the size
+        // of the iterator being spliced in, so we must calculate the start
+        // and end of the range based on program size
+        let (start, end): (usize, usize) = (0x8000, 0x8000 + program.len());
+        self.memory.splice(start..end, program);
 
         // reset vector
         self.memory[0xFFFC] = 0x00;
         self.memory[0xFFFD] = 0x80;
+    }
+}
+
+impl Default for Cpu6502 {
+    fn default() -> Self {
+        // We must fill the memory vec to set the len()
+        // or we'll get index bounds panics in load_program()
+        let memory: Vec<u8> = vec![0; 0xFFFF];
+
+        Self {
+            a: Register::default(),
+            x: Register::default(),
+            y: Register::default(),
+            z: Register::default(),
+            ip: Register::default(),
+            sp: Register::default(),
+            memory,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const PROG: [u8; 4] = [0xDE, 0xAD, 0xBE, 0xEF];
+
+    #[test]
+    fn load_program_into_memory() {
+        let mut cpu = Cpu6502::new();
+        cpu.load_program(PROG.to_vec());
+
+        assert_eq!(cpu.memory[0x8000], 0xDE);
+        assert_eq!(cpu.memory[0x8001], 0xAD);
+        assert_eq!(cpu.memory[0x8002], 0xBE);
+        assert_eq!(cpu.memory[0x8003], 0xEF);
+    }
+
+    #[test]
+    fn set_reset_vector_on_load() {
+        let mut cpu = Cpu6502::new();
+        cpu.load_program(PROG.to_vec());
+
+        assert_eq!(cpu.memory[0xFFFC], 0x00);
+        assert_eq!(cpu.memory[0xFFFD], 0x80);
     }
 }
